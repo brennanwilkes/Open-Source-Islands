ae7ab2582df652731ed141c54ca952f56bb1e666
Brennan Wilkes <brennan@codexwilkes.com>
2020-06-29 16:01:16 -0700

    Added concept art backgrounds

--- handler.js
+++ handler.js
@@ -6,10 +6,24 @@ function setValidMessage(id,message){
 	element.attr("oninput","setCustomValidity(' ')");
 }
 
+let concept_art = [
+	"Tangaroa-Ainalani-Wikolia.png",
+	"Tangaroa-Atalia-Nanai.png",
+	"Tangaroa-Hualani-Among.png",
+	"Tangaroa-Kawene.png",
+	"Tangaroa-Laumidi.png",
+	"Tangaroa-Poivai.png",
+	"Tangaroa-Tau.png",
+	"Tangaroa-Timoteo-Savali.png",
+];
+
 $(document).ready(function(){
 	setValidMessage("name","Island names may only contain letters, spaces hyphens, and apostrophes")
 	setValidMessage("seed","Island Seeds may only contain digits");
 
+	let bod = $("body");
 
+	bod.css("background-image","url('art/"+concept_art[Math.floor(Math.random()*concept_art.length)]+"')")
+	bod.css("background-position",(Math.random() < 0.5 ? "-15" : "65")+"vw "+Math.floor(Math.random()*50)+"vw")
 
 });
