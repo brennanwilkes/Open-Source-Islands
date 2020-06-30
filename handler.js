56eb36cd69488b96bfad0eb6a3ff8697b969f96b
Brennan Wilkes <brennan@codexwilkes.com>
2020-06-29 19:23:18 -0700

    Background redo and animations

--- handler.js
+++ handler.js
@@ -1,10 +1,21 @@
-
 function setValidMessage(id,message){
 	let element = $("#"+id);
 	element.attr("oninvalid","this.setCustomValidity('"+message+"')");
 	element.attr("onchange","try{setCustomValidity('')}catch(e){}");
 	element.attr("oninput","setCustomValidity(' ')");
 }
+
+function spawnParticle(event){
+	let particle = $("<div class=Particle></div>");
+	particle.css("left",Math.random()*99+"vw");
+	particle.css("top",Math.random()*99+"vh");
+	$("body").prepend(particle)
+	console.log(particle)
+	setTimeout(function(event){
+		particle.remove();
+	}, 2000);
+}
+
 
 let concept_art = [
 	"Tangaroa-Ainalani-Wikolia.png",
@@ -21,9 +32,10 @@ $(document).ready(function(){
 	setValidMessage("name","Island names may only contain letters, spaces hyphens, and apostrophes")
 	setValidMessage("seed","Island Seeds may only contain digits");
 
-	let bod = $("body");
+	//let bod = $("body");
+	//bod.css("background-image","url('art/"+concept_art[Math.floor(Math.random()*concept_art.length)]+"')")
+	//bod.css("background-position",(Math.random() < 0.5 ? "-30" : "130")+"% "+Math.floor(Math.random()*30-15)+"vw")
 
-	bod.css("background-image","url('art/"+concept_art[Math.floor(Math.random()*concept_art.length)]+"')")
-	bod.css("background-position",(Math.random() < 0.5 ? "-30" : "130")+"% "+Math.floor(Math.random()*30-15)+"vw")
+	setInterval(spawnParticle, 75);
 
 });
