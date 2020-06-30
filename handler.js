ca17fc6f8e10434c4bf44bf9996c613af41e8deb
Brennan Wilkes <brennan@codexwilkes.com>
2020-06-29 19:37:13 -0700

    More anims and cleanup

--- handler.js
+++ handler.js
@@ -10,32 +10,33 @@ function spawnParticle(event){
 	particle.css("left",Math.random()*99+"vw");
 	particle.css("top",Math.random()*99+"vh");
 	$("body").prepend(particle)
-	console.log(particle)
 	setTimeout(function(event){
 		particle.remove();
 	}, 2000);
 }
 
 
-let concept_art = [
-	"Tangaroa-Ainalani-Wikolia.png",
-	"Tangaroa-Atalia-Nanai.png",
-	"Tangaroa-Hualani-Among.png",
-	"Tangaroa-Kawene.png",
-	"Tangaroa-Laumidi.png",
-	"Tangaroa-Poivai.png",
-	"Tangaroa-Tau.png",
-	"Tangaroa-Timoteo-Savali.png",
-];
-
 $(document).ready(function(){
 	setValidMessage("name","Island names may only contain letters, spaces hyphens, and apostrophes")
 	setValidMessage("seed","Island Seeds may only contain digits");
 
-	//let bod = $("body");
-	//bod.css("background-image","url('art/"+concept_art[Math.floor(Math.random()*concept_art.length)]+"')")
-	//bod.css("background-position",(Math.random() < 0.5 ? "-30" : "130")+"% "+Math.floor(Math.random()*30-15)+"vw")
-
 	setInterval(spawnParticle, 75);
+
+	$("#atoll").change(function() {
+		if(this.checked){
+			$("#volcano").prop("checked", false);
+			$("#motu").prop("checked", true);
+		}
+	});
+	$("#volcano").change(function() {
+		if(this.checked){
+			$("#atoll").prop("checked", false);
+		}
+	});
+	$("#motu").change(function() {
+		if(!this.checked && $("#atoll")[0].checked){
+			$("#motu").prop("checked", true);
+		}
+	});
 
 });
