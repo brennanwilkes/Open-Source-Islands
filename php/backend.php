37dc926fba53e3c771f7d3f671993c04a631016a
Brennan Wilkes <brennan@codexwilkes.com>
2020-07-12 12:40:49 -0700

    does it work?

--- php/backend.php
+++ php/backend.php
@@ -30,48 +30,42 @@
 		global $pdo;
 
 		//setup sql statement
-/*
 
-		$sql = "INSERT INTO islands (seed, name, colour_background, deep_ocean, shallow_ocean, land_one, land_two, land_three, beach, rock_one, rock_two, lava_one, lava_two, village, sunset, has_motu, has_reef, is_volcano, has_town, has_trees, tree_amt, isl_persist, isl_lac, isl_scale, image_data, submission_date, filename) VALUES (:seedVAL, :nameVAL, :colour_backgroundVAL, :deep_oceanVAL, :shallow_oceanVAL, :land_oneVAL, :land_twoVAL, :land_threeVAL, :beachVAL, :rock_oneVAL, :rock_twoVAL, :lava_oneVAL, :lava_twoVAL, :villageVAL, :sunsetVAL, :has_motuVAL, :has_reefVAL, :is_volcanoVAL, :has_townVAL, :has_treesVAL, :tree_amtVAL, :isl_persistVAL, :isl_lacVAL, :isl_scaleVAL, :image_dataVAL, :submission_dateVAL, :filenameVAL) ";
+
+		$sql = "INSERT INTO islands (seed, name, colour_background, deep_ocean, shallow_ocean, land_one, land_two, land_three, beach, rock_one, rock_two, lava_one, lava_two, sunset, has_motu, has_reef, is_volcano, is_atoll, has_town, has_trees, tree_amt, isl_persist, isl_lac, isl_scale, image_data, submission_date, filename) VALUES (:seedVAL, :nameVAL, :colour_backgroundVAL, :deep_oceanVAL, :shallow_oceanVAL, :land_oneVAL, :land_twoVAL, :land_threeVAL, :beachVAL, :rock_oneVAL, :rock_twoVAL, :lava_oneVAL, :lava_twoVAL, :sunsetVAL, :has_motuVAL, :has_reefVAL, :is_volcanoVAL, :is_atollVAL, :has_townVAL, :has_treesVAL, :tree_amtVAL, :isl_persistVAL, :isl_lacVAL, :isl_scaleVAL, :image_dataVAL, :submission_dateVAL, :filenameVAL) ";
 		$statement = $pdo->prepare($sql);
 
 		//bind values
-		$statement->bindValue(":seedVAL",);
-		$statement->bindValue(":nameVAL",);
-		$statement->bindValue(":colour_backgroundVAL",);
-		$statement->bindValue(":deep_oceanVAL",);
-		$statement->bindValue(":shallow_oceanVAL",);
-		$statement->bindValue(":land_oneVAL",);
-		$statement->bindValue(":land_twoVAL",);
-		$statement->bindValue(":land_threeVAL",);
-		$statement->bindValue(":beachVAL",);
-		$statement->bindValue(":rock_oneVAL",);
-		$statement->bindValue(":rock_twoVAL",);
-		$statement->bindValue(":lava_oneVAL",);
-		$statement->bindValue(":lava_twoVAL",);
-		$statement->bindValue(":villageVAL",);
-		$statement->bindValue(":sunsetVAL",);
-		$statement->bindValue(":has_motuVAL",);
-		$statement->bindValue(":has_reefVAL",);
-		$statement->bindValue(":is_volcanoVAL",);
-		$statement->bindValue(":has_townVAL",);
-		$statement->bindValue(":has_treesVAL",);
-		$statement->bindValue(":tree_amtVAL",);
-		$statement->bindValue(":isl_persistVAL",);
-		$statement->bindValue(":isl_lacVAL",);
-		$statement->bindValue(":isl_scaleVAL",);
-		$statement->bindValue(":image_dataVAL",);
-		$statement->bindValue(":submission_dateVAL",);
-		$statement->bindValue(":filenameVAL",);
-
-		$statement->bindValue(':dateVAL', date('Y-m-d'));
-		$statement->bindValue(':moodVAL', $_POST['mood']);
-		$statement->bindValue(':emailVAL', $_POST['email']);
-		$statement->bindValue(':commentTextVAL', $_POST['comment']);
+		$statement->bindValue(":seedVAL",$_POST["seed"]);
+		$statement->bindValue(":nameVAL",$_POST["name"]);
+		$statement->bindValue(":colour_backgroundVAL",isset($_POST["background"]));
+		$statement->bindValue(":deep_oceanVAL",$_POST["ocean"]);
+		$statement->bindValue(":shallow_oceanVAL",$_POST["shallows"]);
+		$statement->bindValue(":land_oneVAL",$_POST["ground1"]);
+		$statement->bindValue(":land_twoVAL",$_POST["ground2"]);
+		$statement->bindValue(":land_threeVAL",$_POST["ground3"]);
+		$statement->bindValue(":beachVAL",$_POST["beach"]);
+		$statement->bindValue(":rock_oneVAL",$_POST["rock1"]);
+		$statement->bindValue(":rock_twoVAL",$_POST["rock2"]);
+		$statement->bindValue(":lava_oneVAL",$_POST["lava1"]);
+		$statement->bindValue(":lava_twoVAL",$_POST["lava2"]);
+		$statement->bindValue(":sunsetVAL",$_POST["name"]);
+		$statement->bindValue(":has_motuVAL",isset($_POST["motu"]));
+		$statement->bindValue(":has_reefVAL",isset($_POST["reef"]));
+		$statement->bindValue(":is_volcanoVAL",isset($_POST["volcano"]));
+		$statement->bindValue(":is_atollVAL",isset($_POST["atoll"]));
+		$statement->bindValue(":has_townVAL",isset($_POST["village"]));
+		$statement->bindValue(":has_treesVAL",isset($_POST["trees"]));
+		$statement->bindValue(":tree_amtVAL",$_POST["tree_amt"]);
+		$statement->bindValue(":isl_persistVAL",$_POST["persistence"]);
+		$statement->bindValue(":isl_lacVAL",$_POST["lacunarity"]);
+		$statement->bindValue(":isl_scaleVAL",$_POST["scale"]);
+		$statement->bindValue(":image_dataVAL","filler");
+		$statement->bindValue(":submission_dateVAL",date('Y-m-d'));
+		$statement->bindValue(":filenameVAL","filler");
 
 		//execute
 		$statement->execute();
-*/
 	}
 
 ?>
