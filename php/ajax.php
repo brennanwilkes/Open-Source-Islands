6ad9722068994b4bb290f98f99a8c9efa02d7668
Brennan Wilkes <brennan@codexwilkes.com>
2020-07-22 14:26:41 -0700

    please just work

--- php/ajax.php
+++ php/ajax.php
@@ -6,10 +6,9 @@
 	if(isset($_POST["id"]) && isset($_POST["request"])){
 
 		$sql = $pdo->prepare("SELECT :reqParam FROM islands WHERE id=:idParam");
-		$sql->bindValue(":reqParam", $_POST["request"]);
+		$sql->bindValue(":reqParam", $_POST["request"],PDO::PARAM_STR);
 		$sql->bindValue(":idParam", $_POST["id"],PDO::PARAM_INT);
 		$sql->execute();
-
 
 		$res = $sql->fetch(PDO::FETCH_ASSOC);
 		echo "name= ".$res[$_POST["request"]];
