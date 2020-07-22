d8602abe34d7e0c2cf3ecd04f1303f8f0e1a05e4
Brennan Wilkes <brennan@codexwilkes.com>
2020-07-22 13:52:36 -0700

    why does it work now??

--- php/ajax.php
+++ php/ajax.php
@@ -7,10 +7,10 @@
 
 	if(isset($_POST["id"]) && isset($_POST["request"])){
 
-		//$sql = $pdo->prepare("SELECT :reqParam FROM islands WHERE id=:idParam");
-		//$sql->bindParam(":reqParam", $_POST["request"], PDO::PARAM_STR);
-		//$sql->bindParam(":idParam", $_POST["id"], PDO::PARAM_STR);
-		//$sql->execute();
+		$sql = $pdo->prepare("SELECT :reqParam FROM islands WHERE id=:idParam");
+		$sql->bindParam(":reqParam", $_POST["request"], PDO::PARAM_STR);
+		$sql->bindParam(":idParam", $_POST["id"], PDO::PARAM_STR);
+		$sql->execute();
 
 
 		echo "did it work";
