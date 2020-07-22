8793798a949f6037e99d9cd058e250a896569f16
Brennan Wilkes <brennan@codexwilkes.com>
2020-07-22 14:39:04 -0700

    weird

--- php/ajax.php
+++ php/ajax.php
@@ -6,7 +6,7 @@
 	if(isset($_POST["id"]) && isset($_POST["request"])){
 
 		$sql = $pdo->prepare("SELECT :reqParam FROM islands WHERE id=:idParam");
-		$sql->bindValue(":reqParam", $_POST["request"]);
+		$sql->bindValue(":reqParam", $_POST["request"],PDO::PARAM_INT);
 		$sql->bindValue(":idParam", (int)$_POST["id"],PDO::PARAM_INT);
 		$sql->execute();
 
