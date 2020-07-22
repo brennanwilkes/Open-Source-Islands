1985d0c65be7c2b3c1e7588de1da9592fca648e8
Brennan Wilkes <brennan@codexwilkes.com>
2020-07-22 13:21:54 -0700

    more sql pdo ahghgh

--- php/ajax.php
+++ php/ajax.php
@@ -3,9 +3,19 @@
 		$id = $_POST["id"];
 		$req = $_POST["mode"];
 
-		$sql = "SELECT ? FROM islands WHERE id=?";
-		$pdo->prepare($sql)->execute([$req,$id]);
-		echo "suc";
+		$sql = "SELECT :req FROM islands WHERE id=:id";
+
+		$reponse = $pdo->query($sql);
+		$reponse->bindParam(':req', $req, PDO::PARAM_STR);
+		$reponse->bindParam(':id', $id, PDO::PARAM_STR);
+		$reponse->execute();
+		$result = $reponse->fetchAll(); //make the select
+		echo $result;
+
+
+
+
+
 	}
 	else{
 		die(json_encode(array("message"=>"ERROR","code"=>422)));
