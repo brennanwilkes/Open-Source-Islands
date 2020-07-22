34695c474893b15d19462a3db3321cd10c4beaac
Brennan Wilkes <brennan@codexwilkes.com>
2020-07-22 13:05:29 -0700

    huh???

--- php/ajax.php
+++ php/ajax.php
@@ -1,8 +1,8 @@
 <?php
 	echo "suc";
-	foreach ($_POST as $key => $value){
-		echo "{$key} = {$value}\r\n";
-	}
+	//foreach ($_POST as $key => $value){
+	//	echo "{$key} = {$value}\r\n";
+	//}
 	echo $_POST["id"];
-	echo $_POST["mode"].;
+	echo $_POST["mode"];
 ?>
