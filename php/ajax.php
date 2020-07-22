ea0bb977985cea404741b51beab566db05c78011
Brennan Wilkes <brennan@codexwilkes.com>
2020-07-22 13:38:55 -0700

    huh

--- php/ajax.php
+++ php/ajax.php
@@ -7,7 +7,7 @@
 
 	if(isset($_POST["id"]) && isset($_POST["request"])){
 
-		$sql = $pdo->prepare("SELECT :reqParam FROM islands WHERE id=:idParam");
+		//$sql = $pdo->prepare("SELECT :reqParam FROM islands WHERE id=:idParam");
 		//$sql->bindParam(":reqParam", $_POST["request"], PDO::PARAM_STR);
 		//$sql->bindParam(":idParam", $_POST["id"], PDO::PARAM_STR);
 		//$sql->execute();
