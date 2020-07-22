3b6ded3b93ee6a18537ca8a9d6f92efae06965ac
Brennan Wilkes <brennan@codexwilkes.com>
2020-07-22 13:08:50 -0700

    does it die?

--- php/ajax.php
+++ php/ajax.php
@@ -1,8 +1,13 @@
 <?php
-	echo "suc";
-	//foreach ($_POST as $key => $value){
-	//	echo "{$key} = {$value}\r\n";
-	//}
-	echo $_POST["id"];
-	echo $_POST["mode"];
+	if(isset($_POST["id"], $_POST["mode"])){
+		$idreq = $_POST["id"];
+		$md = $_POST["mode"];
+	}
+	else{
+		die({
+			message: "ERROR",
+			code: 422
+		});
+	}
+
 ?>
